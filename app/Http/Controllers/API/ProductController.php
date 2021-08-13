e382a7afc4f819e382dadb17d945ddf7352c1f52
Azharu Anwar <azharu.anwar@gmail.com>
2021-08-13 23:55:29 +0700

    Api Product

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -14,5 +16,59 @@ class ProductController extends Controller
         $name = $req->input('id');
         $description = $req->input('description');
         $tags = $req->input('tags');
+        $categories = $req->input('categories');
+
+        $price_from = $req->input('price_form');
+        $price_to = $req->input('price_to');
+
+        if ($id) {
+            $product = Product::with(['category', 'galleries'])->find($id);
+
+            if ($product) {
+                return ResponseFormatter::success(
+                    $product,
+                    'Data produk berhasil diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data produk tidak ada',
+                    404
+                );
+            }
+
+            $product = Product::with(['category', 'galleries']);
+
+            if ($name) {
+                $product->where('name', 'like', '%' . $name . '%');
+            }
+
+            if ($description) {
+                $product->where('name', 'like', '%' . $description . '%');
+            }
+
+            if ($tags) {
+                $product->where('name', 'like', '%' . $tags . '%');
+            }
+
+            if ($price_from) {
+                $product->where('price', '>=', $price_from);
+            }
+
+            if ($price_to) {
+                $product->where('price', '<=', $price_to);
+            }
+
+            if ($categories) {
+                $product->where('categories', '=', $$categories);
+            }
+
+            // dd($product);
+
+            return ResponseFormatter::success(
+                $product->paginate($limit),
+                'Data produk berhasil diambil'
+            );
+        }
     }
 }
